082238a84bb186e5d6c0b526c05feeea1ffad7eb
Agustin Castro <agustin.castro.91@gmail.com>
2017-03-22 18:47:45 -0300

    Added first container component for the todo app

--- app/app.jsx
+++ app/app.jsx
@@ -1,11 +1,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-//Destructuring syntax, each one of the object's attribute is the same as
-//including "var Route = require('react-router').Route;""
-var {Route, Router, IndexRoute, hashHistory} = require('react-router');
-var Main = require('Main');
-var Index = require('Index');
-var About = require('About');
+
+var TodoApp = require('TodoApp');
 
 // Load foundation
 $(document).foundation();
@@ -14,11 +10,6 @@ $(document).foundation();
 require('style!css!sass!applicationStyles');
 
 ReactDOM.render(
-  <Router history={hashHistory}>
-    <Route path="/" component={Main}>
-      <Route path="about" component={About}/>
-      <IndexRoute component={Index}/>
-    </Route>
-  </Router>,
+  <TodoApp/>,
   document.getElementById('app')
 );
---
+++ app/components/TodoApp.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var TodoApp = React.createClass({
+
+  render: function(){
+    return(
+    <div>todo component</div>
+    );
+  }
+
+});
+
+module.exports = TodoApp;
