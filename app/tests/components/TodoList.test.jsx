ed98f4bf8bda4c5938a5744029c6a6ade860c1a7
Agustin Castro <agustin.castro.91@gmail.com>
2017-03-23 11:16:39 -0300

    Created test suite for list component

---
+++ app/tests/components/TodoList.test.jsx
@@ -0,0 +1,37 @@
+var React  = require("react");
+var ReactDOM = require('react-dom');
+var TestUtils = require('react-addons-test-utils');
+var expect = require('expect');
+var $ = require('jQuery');
+
+var TodoList = require('TodoList');
+var Todo = require('Todo');
+
+describe('TodoList', ()=>{
+  it('should exist', ()=> {
+    expect(TodoList).toExist();
+  });
+
+  it('should render on todo component for each todo item', () => {
+    var todos = [
+      {
+        id: 1,
+        text: 'Do something'
+      },
+      {
+        id: 2,
+        text: 'Check mail'
+      }
+    ];
+
+    var todoList = TestUtils.renderIntoDocument(<TodoList todos={todos}/>);
+    //scryRenderedComponentsWithType lets us check how many of a given components are rendered under another component
+    var todosComponents = TestUtils.scryRenderedComponentsWithType(todoList, Todo);
+
+    expect(todosComponents.length).toBe(todos.length);
+
+
+  });
+
+
+});
