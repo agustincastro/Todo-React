9e0d1079b874be1e056d7f9575219b835a81bf5f
Agustin Castro <agustin.castro.91@gmail.com>
2017-03-24 15:08:00 -0300

    Styled app with foundation

--- app/tests/components/TodoList.test.jsx
+++ app/tests/components/TodoList.test.jsx
@@ -27,10 +27,16 @@ describe('TodoList', ()=>{
     var todoList = TestUtils.renderIntoDocument(<TodoList todos={todos}/>);
     //scryRenderedComponentsWithType lets us check how many of a given components are rendered under another component
     var todosComponents = TestUtils.scryRenderedComponentsWithType(todoList, Todo);
-
     expect(todosComponents.length).toBe(todos.length);
+  });
 
 
+  it('should render empty message if there are no todods', () => {
+    var todos = [];
+
+    var todoList = TestUtils.renderIntoDocument(<TodoList todos={todos}/>);
+    var $el = $(ReactDOM.findDOMNode(todoList));
+    expect($el.find('.container__message').length).toBe(1);
   });
 
 
