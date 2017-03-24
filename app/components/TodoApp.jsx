a5097189b8d99612ca923c0a8e63c01bd0c4281a
Agustin Castro <agustin.castro.91@gmail.com>
2017-03-24 12:33:15 -0300

    completed and created timestamps with moment

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -3,6 +3,8 @@ var TodoList =require('TodoList');
 var AddTodo = require('AddTodo');
 var TodoSearch = require('TodoSearch');
 var uuid = require('node-uuid');
+var moment = require('moment');
+
 var TodoAPI = require('TodoAPI');
 
 var TodoApp = React.createClass({
@@ -26,7 +28,9 @@ var TodoApp = React.createClass({
         {
           id: uuid(),
           text: text,
-          completed: false
+          completed: false,
+          createdAt: moment().unix(),
+          completedAt: undefined
         }
       ]
     });
@@ -43,6 +47,7 @@ var TodoApp = React.createClass({
     var updatedTodos = this.state.todos.map((todo)=>{
       if(todo.id === id){
         todo.completed = !todo.completed;
+        todo.completedAt = todo.completed ? moment().unix() : undefined;
       }
       return todo;
     });
