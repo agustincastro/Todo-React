403329c8069d52e26d071df6c0b6a9ef09c2478b
Agustin Castro <agustin.castro.91@gmail.com>
2017-03-27 11:53:36 -0300

    Started adding Redux, reducers and actions

---
+++ app/actions/actions.jsx
@@ -0,0 +1,32 @@
+
+
+export var setSearchText = (searchText) => {
+  return{
+    type: 'SET_SEARCH_TEXT',
+    searchText
+  };
+};
+
+
+export var toggleShowCompleted = () => {
+  return {
+    type: 'TOGGLE_SHOW_COMPLETED',
+  };
+};
+
+
+
+export var toggleTodo = (id) => {
+  return {
+    type: 'TOGGLE_TODO',
+    id
+  };
+};
+
+
+export var addTodo = (searchText) => {
+  return {
+    type: 'ADD_TODO',
+    searchText
+  };
+};
---
+++ app/tests/reducers/reducers.test.jsx
@@ -0,0 +1,29 @@
+
+var expect = require('expect');
+var df = require('deep-freeze-strict');
+
+var reducers = require('reducers');
+
+describe('Reducers', ()=> {
+  describe('searchTextReducer', ()=>{
+
+    it('Should set textSearch', ()=>{
+      var action = {
+        type: 'SET_SEARCH_TEXT',
+        searchText: 'dog'
+      };
+      var res = reducers.searchTextReducer(df(''), df(action));
+      expect(res).toEqual(action.searchText);
+    });
+
+    it('Should toggle show completed', ()=>{
+      var action = {
+        type: 'TOGGLE_SHOW_COMPLETED'
+      };
+      var res = reducers.showCompletedReducer(df(false), df(action));
+      expect(res).toEqual(true);
+    });
+
+
+  });
+});
