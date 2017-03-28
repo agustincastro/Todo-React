c5ffbb341c64b5db3f3eb8b6e885ab6077ca2b8d
Agustin Castro <agustin.castro.91@gmail.com>
2017-03-28 12:03:29 -0300

    refactored todosearch to work with redux

--- app/components/TodoList.jsx
+++ app/components/TodoList.jsx
@@ -1,23 +1,22 @@
 var React = require('react');
 var {connect} = require('react-redux');
 import Todo from 'Todo';
+var TodoAPI = require('TodoAPI');
 
 export var TodoList = React.createClass({
 
-   render: function(){
-
-     var {todos} = this.props;
-     var renderTodos = () => {
-       if(todos.length === 0){
-         return (<p className='container__message'>Nothing to do</p>)
-       }
-       return todos.map((todo) => {
-         return(
-           <Todo key={todo.id}  {...todo}/>
-           );
-         });
-       };
-
+  render: function(){
+    var {todos, showCompleted, searchText} = this.props;
+    var renderTodos = () => {
+      if(todos.length === 0){
+        return (<p className='container__message'>Nothing to do</p>)
+      }
+      return TodoAPI.filterTodos(todos, showCompleted, searchText).map((todo) => {
+        return(
+          <Todo key={todo.id}  {...todo}/>
+        );
+      });
+    };
 
     return(
       <div>
@@ -32,8 +31,6 @@ export var TodoList = React.createClass({
 // The first argument of connect lets us specify which part of the state the component wants to access
 export default connect(
   (state) => {
-    return {
-      todos: state.todos
-    };
+    return state;
   }
 )(TodoList);
